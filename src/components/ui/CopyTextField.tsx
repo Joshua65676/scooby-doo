aedc6036c96c32034c2ba7602c12ffc45f832bb5
Okoro Joshua <Joshua65676@gmail.com>
2024-10-03 22:39:00 +0100

    first commit

--- src/components/ui/CopyTextField.tsx
+++ src/components/ui/CopyTextField.tsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 import { Button } from "./Button";
 
 const CopyTextField = () => {
-  const [text, setText] = useState("");
+  const walletAddress = "vnmYuE9s9PU0dQwXss8YlXx0i92OWMYmFZrDGnjENwDJ";
+  const [text, setText] = useState(false);
 
   const handleCopy = async () => {
     try {
-      await navigator.clipboard.writeText(text);
-      alert("Text copied to clipboard!");
+      await navigator.clipboard.writeText(walletAddress);
+      setText(true);
+      setTimeout(() => setText(false), 2000);
     } catch (err) {
       console.error("Failed to copy text: ", err);
     }
@@ -17,21 +19,20 @@ const CopyTextField = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter Address Here...."
-        className=" p-2 pl-5 h-[60px] xl:w-[650px] md:w-[650px] mx:w-[370px] rounded-2xl bg-primary text-tertiary uppercase font-cinema text-[14px]"
-      />
-      
-        <Button
-          onClick={handleCopy}
-          className="w-[90px] h-[40px] bg-tertiary rounded-xl absolute -ml-28 mt-2"
-        >
-          <span className="text-primary text-[17px] font-bold font-cinema uppercase">Copy</span>
-        </Button>
-      
+      <div className=" p-5 pr-24 xl:h-[60px] md:h-[60px] mx:h-[100px] xl:w-[660px] md:w-[660px] mx:w-[370px] focus:outline-none rounded-2xl bg-primary text-tertiary uppercase font-cinema text-[14px]">
+        <div className="mx:w-[215px] xl:w-[650px] md:w-[650px]">
+          <span className="mx:break-all flex text-left">{walletAddress}</span>
+        </div>
+      </div>
+
+      <Button
+        onClick={handleCopy}
+        className="w-[95px] h-[40px] bg-tertiary rounded-xl absolute z-40 mx:ml-20 xl:ml-[14rem] md:ml-[14rem] xl:-mt-[3.2rem] md:-mt-[3.2rem] mx:-mt-[4.5rem]"
+      >
+        <span className="text-primary text-[17px] font-bold font-cinema uppercase">
+          {text ? "Copyied" : "Copy"}
+        </span>
+      </Button>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config: Config = {
         larken: ['LarkenDemo', 'serif'],
         cinema: ['CinemaCartoon', 'sans-serif'],
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         primary: "#725029",
         tertiary: "#F8C165",
